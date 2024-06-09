f601ec252348406218efd3cf24dd82721750ff3d
bilalashrafali123 <ashrafalibilal385@gmail.com>
2024-06-09 16:39:28 +0500

    finally done

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ console.log(res.data);
 
 res.data.map((item)=>{
        
-div.innerHTML += `<h2>Title : ${item.title}</h2>
+div.innerHTML += `<div class="main-card">
+                  <h2>Title : ${item.title}</h2>
                   <img width ="150" src="${item.image}"  alt="products" /> 
                   <h2>price : ${item.price}</h2>
                   <hr/>
+                  </div>
 `
 
 })
