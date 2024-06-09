c91e73e8736c3ab1829d0f7da1525cb67afcac6d
bilalashrafali123 <ashrafalibilal385@gmail.com>
2024-06-09 16:47:28 +0500

    finally done

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ res.data.map((item)=>{
 div.innerHTML += `<div class="main-card">
                   <h5>Title : ${item.title}</h5>
                   <img width ="100" height="100" src="${item.image}"  alt="products" /> 
-                  <h5>price : ${item.price}</h5><br/>
+                  <h5>price : ${item.price}</h5>
+                  <button type="button" class="btn btn-outline-danger">Danger</button>
+                  <br/>
                   </div>
 `
 
